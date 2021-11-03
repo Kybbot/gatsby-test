7a6daa6cb74f5389f3c29a29e342eb88143b4cd4
Артём Одийко <odiykoartem@gmail.com>
2021-11-03 16:08:30 +0200

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const IndexPage = () => {
+	return (
+		<Layout pageTitle="Home Page">
+			<p>I'm making this by following the Gasby Tutorial.</p>
+		</Layout>
+	);
+};
+
+export default IndexPage;
